61417a6e575759e99a6f00b50c4c423bd85e612d
eveneha <eveneha@stud.ntnu.no>
2024-03-19 19:35:01 +0100

    Copied the current cap1 folder into cap1 on git. Date 19.03.24 @ 19:34 -E

---
+++ cap1/vhdlsim/constants.vhd
@@ -0,0 +1,7 @@
+package CONSTANTS is
+   constant tp_mux : time := 0 ns;
+   constant numBit : integer := 16; 
+   constant IVDELAY : time := 0.1 ns;
+   constant NDDELAY : time := 0.2 ns;
+   constant NRDELAY : time := 0.2 ns;
+end CONSTANTS;
